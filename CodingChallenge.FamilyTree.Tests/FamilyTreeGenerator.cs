65f55840cc6e7b109f01334738565c6179e4b836
Connor MacGregor <macgregoco@gmail.com>
2023-11-08 13:16:22 -0600

    Implement GetBirthMonth function
    Implemented GetBirthMonth function to use BFS. Updated test data generator to always create a tree large enough to support current test cases.

--- CodingChallenge.FamilyTree.Tests/FamilyTreeGenerator.cs
+++ CodingChallenge.FamilyTree.Tests/FamilyTreeGenerator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using CodingChallenge.FamilyTree;
+﻿using System.Linq;
 using FizzWare.NBuilder;
 
 namespace CodingChallenge.FamilyTree.Tests
@@ -13,10 +10,11 @@ namespace CodingChallenge.FamilyTree.Tests
             var hierarchySpec = Builder<HierarchySpec<Person>>.CreateNew()
                 .With(h => h.AddMethod, (p1, p2) => p1.Descendants.Add(p2))
                 .With(h => h.Depth = 7)
-                .With(h => h.MaximumChildren = 3)
+                .With(h => h.MinimumChildren = 2)
+                .With(h => h.MaximumChildren = 4)
                 .With(h => h.NumberOfRoots = 1).Build();
 
-            var person = Builder<Person>.CreateListOfSize(1100).BuildHierarchy(hierarchySpec).First();
+            var person = Builder<Person>.CreateListOfSize(5461).BuildHierarchy(hierarchySpec).First();
 
             return person;
         }
--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CodingChallenge.FamilyTree
 {
     public class Solution
     {
+        //queue based BFS implementation for traversing a family tree to find a descendants birth month
+        //I chose BFS over DFS because it's a little more effecient at scale and it's implemenation is more readable in my opinion
+        //Most BFS implementations ensure graphs can be traversed by maintianing a "visited" structure
+        //I have omitted that here since this is a limited example and our use case probably shouldn't allow that
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            if(person == null 
+                || person.Descendants.Count == 0
+                || string.IsNullOrEmpty(descendantName))
+            {
+                return string.Empty;
+            }
+
+            var treeTraversalQueue = new Queue<Person>();
+
+            treeTraversalQueue.Enqueue(person);
+
+            while(treeTraversalQueue.Any())
+            {
+                var currentFamilyMember = treeTraversalQueue.Dequeue();
+
+                if (currentFamilyMember.Name == descendantName)
+                {
+                    return currentFamilyMember.Birthday.ToString("MMMM");
+                }
+
+                currentFamilyMember.Descendants.ForEach(x => treeTraversalQueue.Enqueue(x));
+            }
+
+            return string.Empty;
         }
     }
 }
