ef70314366a542de6ee116a2793b3575ac4f7528
Connor MacGregor <macgregoco@gmail.com>
2023-11-07 21:31:02 -0600

    Pirate Speak solution
    Implemented checking for anagrams. Implemented helper function to convert strings into sorted character arrays.

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -7,7 +7,28 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            var possibleMatches = dictionary.OfType<string>().ToList(); 
+            var jumbleCharacterArray = StringToSortedCharacterArray(jumble);
+
+            for (int i = possibleMatches.Count - 1; i >= 0; i--)
+            {
+                var possibleMatchArray = StringToSortedCharacterArray(possibleMatches[i]);
+
+                if(!possibleMatchArray.SequenceEqual(jumbleCharacterArray))
+                {
+                    possibleMatches.Remove(possibleMatches[i]);
+                }
+            }
+
+            return possibleMatches.ToArray();
+        }
+
+        private char[] StringToSortedCharacterArray(string inputString)
+        {
+            char[] retVal = inputString.ToLower().ToCharArray();
+            Array.Sort(retVal);
+
+            return retVal;
         }
     }
 }
